966e6720ff0ad3f714f20b9f3e398361dc4641a5
VoroninWorkMachine <voronin@oborot.ru>
2014-09-26 10:02:09 +0400

    Добавлена функция zm.insetContent, которая позволяет после создания модального окна переопределить его контент.

--- js/z_modal.js
+++ js/z_modal.js
@@ -1,156 +1,198 @@
-  /** z_modal v 0.9.9.10
-  * by Alexandr Voronin http://z-site.ru
-  *
-  * @param (str or dom el) content - контент модального окна, принимает строку или ссылку на элемент dom содержимое элемента становится модальным окном
-  * 
-  * @param (obj)options - настройки плагина
-  * @param (str)options.windowType  - fixed(default) - Тип модального окна фиксированное(полоса проктуки не появляется ). notFixed - полоса прокрутки появляется если контент выходит за пределы окна
-  * @param (str)options.windowBackground  - цвет заднего фона по умолчанию black
-  * @param (str)options.windowOpacity  - цвет прозрачность фона по умолчанию 0.5
-  * @param (str)options.windowWidth  - ширина модального окна по умолчанию по содержимому
-  * @param (arr)options.windowPosition  - позиция модального окна относительно окна браузера new Array(x,y) по умолчанию new Array('50%','50%')
-  * 
-  * @param (str)options.windowcloseBackClick  - сыкрывать ли окно по щелчку на заднем фоне по умолчанию true
-  * 
-  * @param (fun)options.closeFunction - функция скрытия модального окна
-  * @param (fun)options.openFunction - функция открытия модального окна  
-  * @param (fun)options.closeFunctionIe - функция скрытия модального окна для IE 6-8
-  * @param (fun)options.openFunctionIe - функция открытия модального окна  для IE 6-8
-  * @param (str)options.closeContent - контент для закрывашки
-  * @param (bul)options.closeShow - показывать ли закрывашку true(def)
-  *
-  * @param (fun) options.hookBeforeCreateWindow - пользовательская функция до создания модального окна
-  * @param (fun) options.hookAfterCreateWindow - пользовательская функция после создания модального окна
-  * 
-  * @param (str)options.idElementPrefix -zm_(default) префикс для элементов модального окна
-  * @param options.zm_* - id блоков модального окна
-  *
-  *
-  * @return zm.closeWindow() - функция которая закрывает окно
-  * @return zm.create() - функция для создания окна
-  *
-  *
-  *
-  */  
+/** z_modal v 0.9.9.10
+ * by Alexandr Voronin http://z-site.ru
+ *
+ * @param (str or dom el) content - контент модального окна, принимает строку или ссылку на элемент dom содержимое элемента становится модальным окном
+ *
+ * @param (obj)options - настройки плагина
+ * @param (str)options.windowType  - fixed(default) - Тип модального окна фиксированное(полоса проктуки не появляется ). notFixed - полоса прокрутки появляется если контент выходит за пределы окна
+ * @param (str)options.windowBackground  - цвет заднего фона по умолчанию black
+ * @param (str)options.windowOpacity  - цвет прозрачность фона по умолчанию 0.5
+ * @param (str)options.windowWidth  - ширина модального окна по умолчанию по содержимому
+ * @param (arr)options.windowPosition  - позиция модального окна относительно окна браузера new Array(x,y) по умолчанию new Array('50%','50%')
+ *
+ * @param (str)options.windowcloseBackClick  - сыкрывать ли окно по щелчку на заднем фоне по умолчанию true
+ *
+ * @param (fun)options.closeFunction - функция скрытия модального окна
+ * @param (fun)options.openFunction - функция открытия модального окна
+ * @param (fun)options.closeFunctionIe - функция скрытия модального окна для IE 6-8
+ * @param (fun)options.openFunctionIe - функция открытия модального окна  для IE 6-8
+ * @param (str)options.closeContent - контент для закрывашки
+ * @param (bul)options.closeShow - показывать ли закрывашку true(def)
+ *
+ * @param (fun) options.hookBeforeCreateWindow - пользовательская функция до создания модального окна
+ * @param (fun) options.hookAfterCreateWindow - пользовательская функция после создания модального окна
+ *
+ * @param (str)options.idElementPrefix -zm_(default) префикс для элементов модального окна
+ * @param options.zm_* - id блоков модального окна
+ *
+ *
+ * @return zm.closeWindow() - функция которая закрывает окно
+ * @return zm.create() - функция для создания окна
+ *
+ *
+ *
+ */
 
-  jQuery.z_modal = function(content,options) {
- var zm = new Object();
- 
-jQuery(function( $ ){
-   zm.content = content;
-   if(typeof content == 'object'){
-	zm.content = $(content).html();
-   }
-   
-  
-  zm.options = $.extend({
-  'windowType':'fixed',
-  'windowBackground':'black',
-  'windowOpacity':'0.5',
-  'windowWidth':'',
-  'windowPosition': new Array('50%','50%'),
-  'windowcloseBackClick':true,
-  'closeFunction':function(mainContainer){$(mainContainer).fadeOut(800);},
-  'openFunction':function(mainContainer){$(mainContainer).fadeIn(800);},
-  'openFunctionIe':function(mainContainer){$(mainContainer).css({'display':'block'});},
-  'closeFunctionIe':function(mainContainer){$(mainContainer).css({'display':'none'});},
-  'closeContent':'<span style="cursor:pointer;" title="close">X</span>',
-  'closeShow':true,
-  'idElementPrefix':'zm_',
-  'hookBeforeCreateWindow':function(){},
-  'hookAfterCreateWindow':function(){}
+jQuery.z_modal = function(content,options) {
+    var zm = new Object();
 
-  },options);
-  
-  $.fn.prefix = function(name){
-   return zm.options.idElementPrefix+name;
-  }
-  var elements = {
-  'zm_main':$(this).prefix('main'),
-  'zm_backfon':$(this).prefix('backfon'),
-  'zm_conteiner_text':$(this).prefix('conteiner_text'),
-  'zm_text':$(this).prefix('text'),
-  'zm_close':$(this).prefix('close'),
-  'zm_styles':$(this).prefix('styles')
-  }
-   
-  
-  zm.options = $.extend(elements,zm.options);
-  zm.is_opera_mobile = function() {
-          if (navigator.appName == 'Opera')
-  {
-    var ua = navigator.userAgent;
-      if ( ua.indexOf("Opera Mobi") > 0) {
-        return true;
-      }
-  }
-  return false;
-        
-    }
-  
+    jQuery(function( $ ){
+        zm.content = content;
+        if(typeof content == 'object'){
+            zm.content = $(content).html();
+        }
 
-  /*Настройки для статичного окна*/
-  zm.windowStyle = {
-  'body':'overflow:hidden;',
-  'containerText':'overflow:auto; position:fixed;'
-  }
-        
-  if (zm.is_opera_mobile()) {
-        zm.windowStyle.body = 'overflow:auto;';
-	zm.windowStyle.containerText = 'position:relative;';
-  }
-  
-  if(zm.options.windowType == 'fixed'){
-	zm.windowStyle.body = '';
-	zm.windowStyle.containerText = 'position:fixed;';
-  }
-  
-  
-  $.fn.openWindow = function(){
-  mainContainer = $('#'+zm.options.zm_main);
-	if(zm.getInternetExplorerVersion()> 0 &&  zm.getInternetExplorerVersion() < 9){
-	zm.options.openFunctionIe(mainContainer);
-	}
-	else {
-	zm.options.openFunction(mainContainer);
-	}
-  }
-  zm.closeWindow = function(){
-  mainContainer = $('#'+zm.options.zm_main);
-	if(zm.getInternetExplorerVersion()> 0 &&  zm.getInternetExplorerVersion() < 9){
-	 zm.options.closeFunctionIe(mainContainer);
-	}
-	else {
-	zm.options.closeFunction(mainContainer);
-	}
-	/**После закрытия окна удаляем все созданные элементы */
-	$('#'+zm.options.zm_styles).remove();
-	$('#'+zm.options.zm_main).remove();
-  }
-  
-  /*Версия IE*/
-  zm.getInternetExplorerVersion = function ()
 
-{
-  var rv = -1;
-  if (navigator.appName == 'Microsoft Internet Explorer')
-  {
-    var ua = navigator.userAgent;
-    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-    if (re.exec(ua) != null)
-      rv = parseFloat( RegExp.$1 );
-  }
-  return rv;
-}
-  /**Создание модального окна, добавляем в body контент*/
-  zm.create = function(){
-if(typeof(zm.options.hookBeforeCreateWindow) == 'function'){
-   zm.options.hookBeforeCreateWindow();
-}
+        zm.options = $.extend({
+            'windowType':'fixed',
+            'windowBackground':'black',
+            'windowOpacity':'0.5',
+            'windowWidth':'',
+            'windowPosition': ['50%','50%'],
+            "windowCloseBackClick":true,
+            'closeFunction':function(mainContainer){$(mainContainer).fadeOut(800);},
+            'openFunction':function(mainContainer){$(mainContainer).fadeIn(800);},
+            'openFunctionIe':function(mainContainer){$(mainContainer).css({'display':'block'});},
+            'closeFunctionIe':function(mainContainer){$(mainContainer).css({'display':'none'});},
+            'closeContent':'<span style="cursor:pointer;" title="close">X</span>',
+            'closeShow':true,
+            'idElementPrefix':'zm_',
+            'hookBeforeCreateWindow':function(){},
+            'hookAfterCreateWindow':function(){}
 
-$('body').prepend(function(index,el){
+        },options);
 
-var s_return =  '<style id="'+zm.options.zm_styles+'">\
+        $.fn.prefix = function(name){
+            return zm.options.idElementPrefix+name;
+        };
+        var elements = {
+            'zm_main':$(this).prefix('main'),
+            'zm_backfon':$(this).prefix('backfon'),
+            'zm_conteiner_text':$(this).prefix('conteiner_text'),
+            'zm_text':$(this).prefix('text'),
+            'zm_close':$(this).prefix('close'),
+            'zm_styles':$(this).prefix('styles'),
+            'zm_content':$(this).prefix('content')
+
+        };
+
+
+        zm.options = $.extend(elements,zm.options);
+        zm.is_opera_mobile = function() {
+            if (navigator.appName == 'Opera')
+            {
+                var ua = navigator.userAgent;
+                if ( ua.indexOf("Opera Mobi") > 0) {
+                    return true;
+                }
+            }
+            return false;
+
+        };
+
+
+        /*Настройки для статичного окна*/
+        zm.windowStyle = {
+            'body':'overflow:hidden;',
+            'containerText':'overflow:auto; position:fixed;'
+        };
+
+        if (zm.is_opera_mobile()) {
+            zm.windowStyle.body = 'overflow:auto;';
+            zm.windowStyle.containerText = 'position:relative;';
+        }
+
+        if(zm.options.windowType == 'fixed'){
+            zm.windowStyle.body = '';
+            zm.windowStyle.containerText = 'position:fixed;';
+        }
+
+
+        $.fn.openWindow = function(){
+            mainContainer = $('#'+zm.options.zm_main);
+            if(zm.getInternetExplorerVersion()> 0 &&  zm.getInternetExplorerVersion() < 9){
+                zm.options.openFunctionIe(mainContainer);
+            }
+            else {
+                zm.options.openFunction(mainContainer);
+            }
+        };
+        zm.closeWindow = function(){
+
+            mainContainer = $('#'+zm.options.zm_main);
+            if(zm.getInternetExplorerVersion()> 0 &&  zm.getInternetExplorerVersion() < 9){
+                zm.options.closeFunctionIe(mainContainer);
+            }
+            else {
+                zm.options.closeFunction(mainContainer);
+            }
+            /**После закрытия окна удаляем все созданные элементы */
+            $('#'+zm.options.zm_styles).remove();
+            $(mainContainer).remove();
+        };
+
+        zm.insetContent = function(content){
+            $('#'+zm.options.zm_content).html(content);
+            zm.alignment();
+        };
+
+
+
+
+        zm.alignment = function(){
+            /*Выравнивание окна по вертикали*/
+            var zmTextSelection =  '#'+zm.options.zm_text;
+            var topSubtraction = $(zmTextSelection).height();
+            topSubtraction = topSubtraction/2;
+            topPadding = $(zmTextSelection).css('top');
+            var _top = parseInt(topPadding) - parseInt(topSubtraction);
+            if (_top < 0) {
+                _top = 0;
+            }
+            $(zmTextSelection).css({'top':_top+'px'});
+
+
+            /*Выравнивание окна по горизонтали*/
+            var text_width = $(zmTextSelection).width();
+            var leftSubtraction = text_width;
+            leftSubtraction = leftSubtraction/2;
+            $(zmTextSelection).css({'left':''});
+            var leftPadding = parseInt($(zmTextSelection).css('left'));
+            var left = leftPadding - leftSubtraction;
+            if (left < 0) {
+                left = 0;
+            }
+            $(zmTextSelection).css({'left': left});
+             $(zmTextSelection).width(text_width);
+
+        };
+
+        /*Версия IE*/
+        zm.getInternetExplorerVersion = function ()
+
+        {
+            var rv = -1;
+            if (navigator.appName == 'Microsoft Internet Explorer')
+            {
+                var ua = navigator.userAgent;
+                var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+                if (re.exec(ua) != null)
+                    rv = parseFloat( RegExp.$1 );
+            }
+            return rv;
+        }
+        /**Создание модального окна, добавляем в body контент*/
+        zm.create = function(){
+            if(typeof(zm.options.hookBeforeCreateWindow) == 'function'){
+                zm.options.hookBeforeCreateWindow();
+            }
+
+            $('body').prepend(function(index,el){
+                var windowsWidthStyle = '';
+                if(zm.options.windowWidth){
+                    windowsWidthStyle = ' width:'+zm.options.windowWidth+';';
+                }
+                var s_return =  '<style id="'+zm.options.zm_styles+'">\
 body {'+zm.windowStyle.body+'}\
 #'+zm.options.zm_main+' {\
 display:none;\
@@ -182,7 +224,7 @@ z-index:100001;\
 *top: expression(document.getElementsByTagName("body")[0].scrollTop + "px");  \
 }\
 #'+zm.options.zm_text+' {\
-    width:'+zm.options.windowWidth+';\
+    '+windowsWidthStyle+'\
     padding:0;\
     margin:0;\
     position:absolute;\
@@ -196,84 +238,65 @@ clean:both;\
 position:;\
 }\
 </style>';
-var s_close ='';
-if (zm.options.closeShow) {
-  //code
+                var s_close ='';
+                if (zm.options.closeShow) {
+                    //code
 
- s_close = '<div align="right" style="position:relative;"><div style="background:red;position:relative;width:0px;"><div style="position:absolute;" id="'+zm.options.zm_close+'">'+zm.options.closeContent+'</div><br></div></div>';
-}
+                    s_close = '<div align="right" style="position:relative;"><div style="background:red;position:relative;width:0px;"><div style="position:absolute;" id="'+zm.options.zm_close+'">'+zm.options.closeContent+'</div><br></div></div>';
+                }
 
-s_return += '<div id="'+zm.options.zm_main+'">\
+                s_return += '<div id="'+zm.options.zm_main+'">\
     <div  id="'+zm.options.zm_backfon+'"></div>\
 	<div  id="'+zm.options.zm_conteiner_text+'">\
-<div id="'+zm.options.zm_text+'" >'+s_close+''+zm.content+'</div>\
+<div id="'+zm.options.zm_text+'" >'+s_close+''+'<div id="'+zm.options.zm_content+'">'+zm.content+'</div></div>\
 	  </div></div>';
-    
-  
-return s_return;
-
-});   
-$(this).openWindow();
-   /**Вызвали функции после создания*/
-	$(this).afterCreate();
-   if(typeof(zm.options.hookAfterCreateWindow) == 'function'){
-      zm.options.hookAfterCreateWindow();
-   }
-}
-/**Действия которые выполняются после создания модального окна*/
-$.fn.afterCreate = function(){
-
-	
-	/**Скрытие окна если происходит клик по фону*/
-	if(zm.options.windowcloseBackClick) {
-	$('#'+zm.options.zm_main).click(function(event)	{
-		var clicked = jQuery(event.target);
-		var is_find = $('#'+zm.options.zm_text).find($(clicked));
-		if(is_find.length == 0 && !(clicked.is('#'+zm.options.zm_text))) {
-				zm.closeWindow();
-		}
-   }); 
-   }
-	
 
 
-/*Выравнивание окна по вертикали*/
-var topSubtraction = $('#'+zm.options.zm_text).height();
-topSubtraction = topSubtraction/2;
- topPadding = $('#'+zm.options.zm_text).css('top');
- _top = parseInt(topPadding) - parseInt(topSubtraction);
-if (_top < 0) {
-     _top = 0;
-}
-$('#'+zm.options.zm_text).css({'top':_top+'px'});
+                return s_return;
+
+            });
+            $(this).openWindow();
+            /**Вызвали функции после создания*/
+            $(this).afterCreate();
+            if(typeof(zm.options.hookAfterCreateWindow) == 'function'){
+                zm.options.hookAfterCreateWindow();
+            }
+        };
+        /**Действия которые выполняются после создания модального окна*/
+        $.fn.afterCreate = function(){
 
 
-	/*Выравнивание окна по горизонтали*/
-  var text_width = $('#'+zm.options.zm_text).width();
-  var leftSubtraction = text_width;
-  leftSubtraction = leftSubtraction/2;
-  var leftPadding = parseInt($('#'+zm.options.zm_text).css('left'));
-  var left = leftPadding - leftSubtraction;
-  if (left < 0) {
-     left = 0;
-  }
+            /**Скрытие окна если происходит клик по фону*/
+            if(zm.options.windowCloseBackClick) {
+                $('#'+zm.options.zm_main).click(function(event)	{
+                    var clicked = jQuery(event.target);
+                    var is_find = $('#'+zm.options.zm_text).find($(clicked));
+                    if(is_find.length == 0 && !(clicked.is('#'+zm.options.zm_text))) {
+                        zm.closeWindow();
+                    }
+                });
+            }
 
-  $('#'+zm.options.zm_text).css({'left': left});	
-  $('#'+zm.options.zm_text).width(text_width); 
 
-/*Добавление прозрачности*/
-$('#'+zm.options.zm_backfon).fadeTo(0,zm.options.windowOpacity);
-/*Добвыление функции скрытия окна на крестик*/
-$('#'+zm.options.zm_close).click(function(){
 
-zm.closeWindow();
 
-});
 
-}
-   /**Создали окно*/
-	zm.create();
 
-});  
-return zm;
-  }
+
+            zm.alignment();
+            /*Добавление прозрачности*/
+            $('#'+zm.options.zm_backfon).fadeTo(0,zm.options.windowOpacity);
+            /*Добвыление функции скрытия окна на крестик*/
+            $('#'+zm.options.zm_close).click(function(){
+
+                zm.closeWindow();
+
+            });
+
+        }
+        /**Создали окно*/
+        zm.create();
+
+    });
+    return zm;
+};
